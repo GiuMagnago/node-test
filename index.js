8b09422f2dd0cd6968a9f377ca11ec2fdc3323a4
Giuseppe Chaves Magnago <giuseppecm2014@gmail.com>
2022-11-30 20:49:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,9 @@ const main = async () => {
 
     await page.waitForSelector('.css-181wc2h > .css-dvxtzn > div > h6')
     
-    const values = await page.$$eval('.css-181wc2h > .css-dvxtzn > div > h6',(el) => el.map((values) => values.innerText))
+    const values = await page.$$eval('.css-181wc2h > .css-dvxtzn > div :nth-child(-n + 2)',(el) => el.map((values) => values.innerText)
     
-    var i = (values[0] <= 1.4 && values[1] <= 1.4) ? "1" : 0;
-    
-    console.log(i)
+    console.log(values)
 
     await browser.close()
   } catch (error) {

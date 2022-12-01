e158bd642fb6e250172491b050d1f5100c9b25ea
Giuseppe Chaves Magnago <giuseppecm2014@gmail.com>
2022-12-01 19:29:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,40 @@
-import fetch from 'node-fetch';
 
-const main = async() => {
-  while(true) {
-    await fetch("https://www.google.com.br/").then((response) => {
-      if (response.status == 200) {
-        console.log("yay");
-      }
-    }, (err) => {
-      console.log("error: " + err); // (currently fetch failed)
-    })
-    
-  }
-}
-
-main();
-
-
-/*
 const pup = require('puppeteer')
 
 const url = 'https://tipmanager.net/en'
 
 const main = async () => {
-  try {
+  while (true) {
+
+    try {
       
-    const browser = await pup.launch();
-    const page = await browser.newPage()
-    console.log('  iniciei')
-
-    await page.setDefaultNavigationTimeout(0);
-    
-    await page.goto(url)
-    console.log('  fui p url')
-
-    await page.waitForSelector('div.css-1yep5kg')
-
-    await page.click('div.css-1yep5kg')
-    await page.click('ul.css-r8u8y9 :nth-child(7)')
-
-    await page.waitForSelector('.css-181wc2h > .css-dvxtzn > div > h6')
-
-    const values = [await page.$eval('.css-181wc2h > .css-dvxtzn > div > h6', el => el.innerText), await page.$eval('.css-181wc2h :nth-child(2) > div > h6', el => el.innerText)]
-
-    console.log(values)
-    
-
-    await browser.close()
-    
-  } catch (error) {
-    console.error(error)
+      const browser = await pup.launch();
+      const page = await browser.newPage()
+      console.log('  iniciei')
+  
+      await page.setDefaultNavigationTimeout(0);
+      
+      await page.goto(url)
+      console.log('  fui p url')
+  
+      await page.waitForSelector('div.css-1yep5kg')
+  
+      await page.click('div.css-1yep5kg')
+      await page.click('ul.css-r8u8y9 :nth-child(7)')
+  
+      await page.waitForSelector('.css-181wc2h > .css-dvxtzn > div > h6')
+  
+      const values = [await page.$eval('.css-181wc2h > .css-dvxtzn > div > h6', el => el.innerText), await page.$eval('.css-181wc2h :nth-child(2) > div > h6', el => el.innerText)]
+  
+      console.log(values)
+      
+  
+      await browser.close()
+      
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
-main()*/
+main()

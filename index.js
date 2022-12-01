835da20825727373a56209b4607bb60119cd526a
Giuseppe Chaves Magnago <giuseppecm2014@gmail.com>
2022-12-01 18:15:24 -0300

    .

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+import fetch from 'node-fetch';
 
+const main = async() => {
+  await fetch("https://www.google.com.br/").then((response) => {
+    console.log(response)
+
+    if (response.status == 200) {
+      console.log("yay");
+    }
+  }, (err) => {
+    console.log("error: " + err); // (currently fetch failed)
+  })
+}
+
+main();
+
+/*
 const pup = require('puppeteer')
 
 const url = 'https://tipmanager.net/en'
@@ -21,8 +37,7 @@ const main = async () => {
 
     await page.waitForSelector('.css-181wc2h > .css-dvxtzn > div > h6')
 
-    const values = await page.$$eval('.css-181wc2h > .css-dvxtzn > div > h6',(el) => el.map((values) => values.innerText))
-
+    const values = [await page.$eval('.css-181wc2h > .css-dvxtzn > div > h6', el => el.innerText), await page.$eval('.css-181wc2h :nth-child(2) > div > h6', el => el.innerText)]
 
     console.log(values)
 
@@ -32,4 +47,4 @@ const main = async () => {
   }
 }
 
-main()
+main()*/

6c738bc03786b9e489ca75eac27aad310f86b0d1
Giuseppe Chaves Magnago <giuseppecm2014@gmail.com>
2022-12-02 10:31:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,15 +21,15 @@ const main = async () => {
   
       await page.waitForSelector('.css-181wc2h > .css-dvxtzn > div > h6')
       
-
-      const values = [await page.$eval('.css-181wc2h > .css-dvxtzn > div > h6', el => el.innerText), await page.$eval('.css-181wc2h :nth-child(2) > div > h6', el => el.innerText)]
+      while (true) {
+         setTimeout(async() => {
+            const values = [await page.$eval('.css-181wc2h > .css-dvxtzn > div > h6', el => el.innerText), await page.$eval('.css-181wc2h :nth-child(2) > div > h6', el => el.innerText)]
   
-      console.log(values)
-      
+            console.log(values)
+      }, 1000 * 20)
       
   
       await browser.close()
-      main();
     } catch (error) {
       console.error(error)
     }
